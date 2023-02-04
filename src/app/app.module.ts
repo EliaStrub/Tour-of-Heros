a1661b61d5bb9678f071f544b7700ce8e2118a44
Elia <elia.strub@lernen.siw.swiss>
2023-02-04 15:16:45 +0100

    https://angular.io/tutorial/tour-of-heroes/toh-pt1 Add a hero property Show the hero Show the HeroesComponent view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule,
